2f125b2d6167c87ee624d521d4553a52b66d2178
andrew-barrett <ABarrett@qa.com>
2021-06-07 16:19:51 +0100

    added generate button to service 1

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,8 +1,14 @@
 from application import app, db
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request
+from application.models import Form
+import requests
 
 @app.route('/', methods = ['GET', 'POST'])
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/home', methods = ['GET', 'POST'])
 def home():
+    form = Form()
+    if request.method == "POST":
+        name = requests.get('http://service2:5000/name').text
+        return f"{name}"
     
-    return render_template('index.html')
+    return render_template('index.html', form=form)
