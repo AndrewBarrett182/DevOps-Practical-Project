534e8066ae327dd191aa7d363ffa3229798175f3
andrew-barrett <ABarrett@qa.com>
2021-06-08 11:03:54 +0100

    created service 3

---
+++ service3/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/lottery')
+def lottery():
+    numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    winning_numbers = []
+    for i in range(6):
+        generate = random.choice(numbers)
+        winning_numbers.append(generate)
+        numbers.remove(generate)
+    return winning_numbers
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
