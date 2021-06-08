820b5d5ea0d59d847e454ca1dfc440f06469f6aa
andrew-barrett <ABarrett@qa.com>
2021-06-08 14:53:00 +0100

    display database to html

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,6 +1,6 @@
 from application import app, db
 from flask import render_template, request
-from application.models import Form
+from application.models import Form, LotteryTickets
 import requests
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -12,6 +12,12 @@ def home():
         lottery = requests.get('http://service3:5002/lottery').json()['winning_numbers']
         send = {'ticket':ticket, 'lottery':lottery}
         prize = requests.post("http://service4:5003/prize", json=send).json()
-        return render_template('index.html', form=form, ticket=ticket, lottery=lottery, prize=prize)
+
+        previous_tickets = LotteryTickets.query.all()
+
+        db.session.add(LotteryTickets(ticket = ticket, winning = lottery, prize = prize))
+        db.session.commit()
+
+        return render_template('index.html', form=form, ticket=ticket, lottery=lottery, prize=prize, previous_tickets = previous_tickets)
     
     return render_template('index.html', form=form)
