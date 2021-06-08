0788a5b6c5a9c10eb867fe7b89c2ff0e03444c57
andrew-barrett <ABarrett@qa.com>
2021-06-08 09:27:42 +0100

    updated service 2

--- service2/app.py
+++ service2/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
-import names
+import random
 
 app = Flask(__name__)
 
-@app.route('/name')
-def name():
-    return names.get_full_name()
+
+
+@app.route('/ticket')
+def ticket():
+    numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    lottery_ticket = []
+    for i in range(6):
+        generate = random.choice(numbers)
+        lottery_ticket.append(generate)
+        numbers.remove(generate)
+    return lottery_ticket
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
