8162105275c5a3bac46ab14ee01ab0b0213196bc
andrew-barrett <ABarrett@qa.com>
2021-06-08 15:47:08 +0100

    test

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -14,11 +14,11 @@ def home():
         prize = requests.post("http://service4:5003/prize", json=send).json()
 
         # db.session.add(LotteryTickets(ticket = json.loads(ticket), lottery = json.loads(lottery), prize = json.loads(prize)))
-        db.session.add(LotteryTickets(ticket = json.loads("".join(str(ticket)))))
-        db.session.commit()
+        # db.session.add(LotteryTickets(ticket = json.loads("".join(str(ticket)))))
+        # db.session.commit()
 
-        previous_tickets = LotteryTickets.query.order_by(LotteryTickets.id.desc()).limit(5).all()
+        # previous_tickets = LotteryTickets.query.order_by(LotteryTickets.id.desc()).limit(5).all()
 
-        return render_template('index.html', form=form, ticket=ticket, lottery=lottery, prize=prize, previous_tickets = previous_tickets)
+        return render_template('index.html', form=form, ticket=ticket, lottery=lottery, prize=prize, previous_tickets = previous_tickets, type = type(ticket))
     
     return render_template('index.html', form=form)
