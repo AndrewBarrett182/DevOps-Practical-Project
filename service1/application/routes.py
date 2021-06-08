40f57f7b4c6f0468cbdc78db0eed7af6b95187ef
andrew-barrett <ABarrett@qa.com>
2021-06-08 10:24:32 +0100

    updated service 1

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -9,6 +9,7 @@ def home():
     form = Form()
     if form.validate_on_submit():
         ticket = requests.get('http://service2:5001/ticket').text
+        #lottery = requests.get('http://service3:5002/lottery').text
         return render_template('index.html', form=form, ticket=ticket)
     
     return render_template('index.html', form=form)
