2614a5e940e79251c230e0c56f0d5777b33b3ae8
andrew-barrett <ABarrett@qa.com>
2021-06-08 12:39:56 +0100

    wrote function for service 4

--- service4/app.py
+++ service4/app.py
@@ -2,9 +2,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+prizes = [0, 5, 10, 30, 100, 10000]
+
 @app.route('/prize', methods=['POST'])
 def prize():
-    return 
+    match = 0
+    ticket = request.json['ticket']
+    lottery = request.json['lottery']
+    for i in range(len(ticket)):
+        if ticket[i] in lottery:
+            match = match + 1
+    return jsonify(prizes[match])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5003)
