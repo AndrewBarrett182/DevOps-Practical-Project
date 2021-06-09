af4360fe968b23b00c9a88289b77d97c05b0e58c
andrew-barrett <ABarrett@qa.com>
2021-06-09 17:01:25 +0100

    fixed the prize list

--- service4/app.py
+++ service4/app.py
@@ -2,18 +2,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-prizes = [0, 5, 10, 30, 100, 10000]
+prizes = [0, 1, 5, 10, 30, 100, 10000]
 
 @app.route('/prize', methods=['POST'])
 def prize():
     match = 0
     ticket = request.json['ticket']
     lottery = request.json['lottery']
-    print(ticket, lottery)
     for i in range(len(ticket)):
         if ticket[i] in lottery:
             match = match + 1
-        print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",match)
     return jsonify(prizes[match])
 
 if __name__ == '__main__':
