4311ddfa6d176d541175b8e003a4197db22ba1d1
andrew-barrett <ABarrett@qa.com>
2021-06-09 16:40:35 +0100

    fixing service 4 tests

--- service4/tests/test_service4.py
+++ service4/tests/test_service4.py
@@ -8,13 +8,15 @@ class TestBase(TestCase):
         app.config.update(DEBUG = True)
         return app
 
-tests = [([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 10000), ([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], 0), ([1, 2, 3, 4, 5, 6], [1, 2, 7, 8, 9, 10], 10)]
+tests = [([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 10000), 
+         ([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], 0), 
+         ([1, 2, 3, 4, 5, 6], [1, 2, 7, 8, 9, 10], 10)]
 
 class TestViews(TestBase):
     def test_get_prize(self):
         for case in tests:
-            with patch("random.choice") as r:
+            with patch("random.sample") as r:
                 send = {'ticket':case[0], 'lottery':case[1]}
                 response = self.client.post(url_for("prize"), json = send)
                 self.assertEqual(response.status_code, 200)
-                self.assertIn(case[2], response.data.decode("utf-8"))
+                self.assertIn(case[2], response.json)
