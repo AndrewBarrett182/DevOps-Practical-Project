f88fd441d65f604ea50e11c34815401ebcb72043
andrew-barrett <ABarrett@qa.com>
2021-06-09 16:14:39 +0100

    fixed format

--- service2/tests/test_service2.py
+++ service2/tests/test_service2.py
@@ -21,4 +21,4 @@ class TestViews(TestBase):
                 r.return_value = case[0]
                 response = self.client.get(url_for("ticket"))
                 self.assertEqual(response.status_code, 200)
-                self.assertIn("".join(str(case[0])), response.data.decode())
+                self.assertIn("[\n    1, \n    2, \n    3, \n    4, \n    5, \n    6\n  ]", response.data.decode())
