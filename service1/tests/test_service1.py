07cf0c6af7d0647109f069c83956a48a1cfdc243
andrew-barrett <ABarrett@qa.com>
2021-06-09 13:25:45 +0100

    fixing test

--- service1/tests/test_service1.py
+++ service1/tests/test_service1.py
@@ -37,9 +37,9 @@ class TestViews(TestBase):
         with requests_mock.Mocker() as m:
             tests = [([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 10000), ([1, 5, 10, 15, 17, 20], [2, 6, 7, 9, 14, 16], 0)]
             for case in tests:
-                m.get('http://service2:5001/ticket', json = case[0])
-                m.get('http://service3:5002/lottery', json = case[1])
+                m.get('http://service2:5001/ticket', json = {'lottery_ticket':case[0]})
+                m.get('http://service3:5002/lottery', json = {'winning_numbers':case[1]})
                 m.post('http://service4:5003/prize', json = case[2])
-                response = self.client.get(url_for('home'))
+                response = self.client.post(url_for('home'))
             self.assertIn(b"Ticket: [1, 2, 3, 4, 5, 6]", response.data)
             self.assertIn(b"Ticket: [1, 5, 10, 15, 17, 20]", response.data)
