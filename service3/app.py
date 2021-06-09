78e5b023fdd1aaefc185e5fef70b4fac41972193
andrew-barrett <ABarrett@qa.com>
2021-06-09 16:38:13 +0100

    fixed service 3 tests

--- service3/app.py
+++ service3/app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 @app.route('/lottery', methods = ['GET'])
 def lottery():
     numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    winning_numbers = []
-    for i in range(6):
-        generate = random.choice(numbers)
-        winning_numbers.append(generate)
-        numbers.remove(generate)
+    winning_numbers = random.sample(numbers, 6)
     winning_numbers.sort()
     return jsonify({'winning_numbers':winning_numbers})
 
--- service3/tests/test_service3.py
+++ service3/tests/test_service3.py
@@ -8,13 +8,16 @@ class TestBase(TestCase):
         app.config.update(DEBUG = True)
         return app
 
-tests = [([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 10000), ([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], 0), ([1, 2, 3, 4, 5, 6], [1, 2, 7, 8, 9, 10], 10)]
+tests = [([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 10000), 
+         ([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], 0), 
+         ([1, 2, 3, 4, 5, 6], [1, 2, 7, 8, 9, 10], 10)]
 
 class TestViews(TestBase):
     def test_get_lottery(self):
+
         for case in tests:
-            with patch("random.choice") as r:
+            with patch("random.sample") as r:
                 r.return_value = case[1]
                 response = self.client.get(url_for("lottery"))
                 self.assertEqual(response.status_code, 200)
-                self.assertIn(case[1], response.data.decode("utf-8"))
+                self.assertEqual(case[1], response.json['winning_numbers'])
