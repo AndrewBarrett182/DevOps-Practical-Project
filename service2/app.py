816869b27c5c8efaddff50df9cce5022ecff08bc
andrew-barrett <ABarrett@qa.com>
2021-06-09 16:03:42 +0100

    testing

--- service2/app.py
+++ service2/app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 @app.route('/ticket', methods = ['GET'])
 def ticket():
     numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    lottery_ticket = []
-    for i in range(6):
-        generate = random.choice(numbers)
-        lottery_ticket.append(generate)
-        numbers.remove(generate)
+    lottery_ticket = random.sample(numbers, 6)
     lottery_ticket.sort()
     return jsonify({'lottery_ticket':lottery_ticket})
 
