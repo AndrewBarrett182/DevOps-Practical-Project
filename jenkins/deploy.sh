4022586a42de69bd455526bbd91130456e3eaee9
andrew-barrett <ABarrett@qa.com>
2021-06-11 14:24:23 +0100

    fixed error

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the docker compose yaml to the manager node
-scp -i ~/.ssh/ansible_id_rsa jenkins@ansible-manager docker-compose.yaml jenkins@ansible-manager:/home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@ansible-manager:/home/jenkins/docker-compose.yaml
 
 # Deploy the Docker Stack
 ssh -i ~/.ssh/ansible_id_rsa jenkins@ansible-manager "docker stack deploy --compose-file docker-compose.yaml lottery"
